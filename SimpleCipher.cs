03cd97fce825b20fe9f4063a8f73d0b65dea528c
KevinYakha <kevin@yakha.eu>
2024-04-15 15:36:58 +0200

    added the forgotten letter back

--- SimpleCipher.cs
+++ SimpleCipher.cs
@@ -58,5 +58,5 @@ public class SimpleCipher
     private string _key;
     private char[] _chars = { 'a', 'b', 'c', 'd', 'e',
         'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o',
-        'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z' };
+        'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 }
